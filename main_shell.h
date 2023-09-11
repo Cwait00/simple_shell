5d888dd573ddf5731c5a550ce4b54dabeeb23f0b
Lilita <lilitambete2911@gmail.com>
2023-09-11 06:46:06 -0700

    simp_shell.c

---
+++ main_shell.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_SHELL_H
+#define MAIN_SHELL_H
+#include <stdio.h>
+#include <stddef.h>
+int main(void);
+
+#endif
---
+++ simp_shell.c
@@ -0,0 +1,9 @@
+#include "main_shell.h"
+/**
+ * main - function for beginning of simple shell
+ * Return: 0 upon success
+ */
+int main(void)
+{
+	return (0);
+}
