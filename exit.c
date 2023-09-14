5d5ea8b34fcd49f01f46e69177d02652b84b75ee
Cwait00 <cwaitanobongoza@gmail.com>
2023-09-14 13:15:13 -0700

    info_t.c

---
+++ exit.c
@@ -0,0 +1,74 @@
+#include "main_shell.h"
+
+/**
+ **_strncpy - String copy
+ *@dest: Destination string
+ *@src: Source string
+ *@n: amount of characters to be copied
+ *Return: concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a, j;
+	char *s = dest;
+
+	a = 0;
+	while (src[a] != '\0' && a < n - 1)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	if (a < n)
+	{
+		j = a;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - Two strings concatenates.
+ *@dest: First string
+ *@src: Second string
+ *@n: amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, j;
+	char *s = dest;
+
+	a = 0;
+	j = 0;
+	while (dest[a] != '\0')
+		a++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[a] = src[j];
+		a++;
+		j++;
+	}
+	if (j < n)
+		dest[a] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - String character
+ *@s: String to be parsed
+ *@c: The character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
