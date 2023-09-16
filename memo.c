956301da20e394736fdd8854f5f69a12ee40aac1
Cwait00 <cwaitanobongoza@gmail.com>
2023-09-16 11:47:16 -0700

    updated

---
+++ memo.c
@@ -0,0 +1,17 @@
+#include "main_shell.h"
+
+/**
+ * bfree - NULLs the address and frees a pointer
+ * @ptr: address of pointer to free
+ * Return: 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
