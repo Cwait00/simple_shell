49ce4da2cbf60a11daedffb21236c91d610fa0b6
Cwait00 <cwaitanobongoza@gmail.com>
2023-09-21 17:31:21 -0700

    updated

--- atoi.c
+++ atoi.c
@@ -1,112 +1,73 @@
-<<<<<<< HEAD
-#include "main_shell.h"
-=======
 #include "maint_shell.h"
 
-
->>>>>>> 63fe436cd35645e6351e63238eb005f7839b9733
 /**
- * interactive - Shell is interactive mode and returns if true
- * @info: struct address
+ * interactive - checks if the file is running interactive mode
+ * @info: structure
  * Return: 1 if interactive mode, 0 otherwise
  */
-<<<<<<< HEAD
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
-
-=======
-int interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
->>>>>>> 63fe436cd35645e6351e63238eb005f7839b9733
 
 /**
- * is_delim - checks if is a delimeter
- * @c: The charecter to check
- * @delim: The string of the delimeter
+ * is_delim - checks ifa given character is a delimeter
+ * @c: the character to check
+ * @delim: the delimeter
  * Return: 1 if true, 0 if false
  */
-<<<<<<< HEAD
-int is_delim(char c, char *delim)
-
-======
 int is_delim(char c, char *delim)
 {
->> >> >> 63fe436cd35645e6351e63238eb005f7839b9733
 	while (*delim)
 		if (*delim++ == c)
 			return (1);
 	return (0);
 }
-<< << << HEAD
-
-== == ==
->> >> >> 63fe436cd35645e6351e63238eb005f7839b9733
 
 /**
- * _isalpha - Alphabetic character
+ * _isalpha - checks if a given character is an alphabetic
  * @c: The character to input
  * Return: 1 if c is alphabetic, 0 otherwise
  */
 
-<< << << HEAD
-int _isalpha(int c)
-
-== == ==
 int _isalpha(int c)
 {
->> >`> >> 63fe436cd35645e6351e63238eb005f7839b9733
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
-<< << << HEAD
-
-== == ==
->> >> >> 63fe436cd35645e6351e63238eb005f7839b9733
 
 /**
- * _atoi - A string to an integer
- * @s: String to be converted
- * Return: 0 if no num in string, converted number otherwise
+ * _atoi - converts string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
  */
 
-<< << << HEAD
-int _atoi(char *s)
-== == ==
 int _atoi(char *s)
 {
->> >> >> 63fe436cd35645e6351e63238eb005f7839b9733
-	int i, sign = 1, flag = 0, output;
-	unsigned int r = 0;
+	int c, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	for (c = 0; s[c] != '\0' && flag != 2; i++)
 	{
-		if (s[i] == '-')
+		if (s[c] == '-')
 			sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[c] >= '0' && s[c] <= '9')
 		{
 			flag = 1;
-			r *= 10;
-			r += (s[i] - '0');
+			result *= 10;
+			result += (s[c] - '0');
 		}
 		else if (flag == 1)
 			flag = 2;
 	}
 
 	if (sign == -1)
-		output = -r;
+		output = -result;
 	else
-		output = r;
+		output = result;
 
 	return (output);
 }
-<< << << HEAD
-
-== == ==
->> >> >> 63fe436cd35645e6351e63238eb005f7839b9733
