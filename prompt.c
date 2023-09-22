7f67b61f35ae1856e381397eb9b132d8b51d5464
Lilita <lilitambete2911@gmail.com>
2023-09-22 16:59:01 -0700

    parsing.c removed

--- prompt.c
+++ prompt.c
@@ -11,6 +11,7 @@ int prompt_nexit(int argc, char **argv)
 	char *shell_lineptr = NULL;
 	char interpreter[] = "simple_shell: ";
 	size_t n = 0;
+	size_t user_input = getline(&shell_lineptr, &n, stdin);
 	char *program_name = argv[0];
 
 	(void)argc;
@@ -19,19 +20,20 @@ int prompt_nexit(int argc, char **argv)
 	{
 		_puts(interpreter);
 
-		if (getline(&shell_lineptr, &n, stdin) == -1)
+		if (user_input == (size_t)-1)
 		{
 			/*if (feof(stdin))*/
 			if (feof(stdin))
-			{/*handling the ctrl + D*/
+			{handling the ctrl + D
 				break;
 			}
 			perror(program_name);
-			return (1);
+			return (-1);
 		}
 		_puts(shell_lineptr);
 		/*because getline allocates memory, it must be freed*/
 		shell_lineptr = NULL;
+		free(shell_lineptr);
 	}
 	/*free again for any memory not freed before*/
 	if (shell_lineptr != NULL)
